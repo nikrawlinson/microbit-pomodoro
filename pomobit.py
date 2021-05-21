33cb0bd5cd1004ee8e8d8a81bdfe23f8dfd6db0d
nikrawlinson <70710354+nikrawlinson@users.noreply.github.com>
2021-05-21 17:57:49 +0100

    Update pomobit.py

--- pomobit.py
+++ pomobit.py
@@ -2,7 +2,7 @@ import microbit
 
 def workcycle():
     
-    microbit.display.scroll("Time to work")
+    microbit.display.scroll("W")
     microbit.sleep(1000)
     for y in range(5):
         for x in range(5):
@@ -11,37 +11,44 @@ def workcycle():
         for x in reversed(range(5)):
             for stall in range(6000):
                 microbit.sleep(10)
-                if microbit.button_a.was_pressed():
-                    waiting()
                 if microbit.button_b.was_pressed():
+                    return
+                if microbit.button_a.was_pressed():
                     return
             microbit.display.set_pixel(x, y, 0)
     microbit.sleep(1000)
         
 def restcycle():
 
-    microbit.display.scroll("Take a break")
+    microbit.display.scroll("B")
     microbit.sleep(1000)
     for t in reversed(range(5)):
         time = t + 1
         microbit.display.show(time)
         for stall in range(6000):
             microbit.sleep(10)
-            if microbit.button_a.was_pressed():
-                waiting()
             if microbit.button_b.was_pressed():
+                return
+            if microbit.button_a.was_pressed():
                 return
     microbit.display.clear()
     microbit.sleep(1000)
     
 def waiting():
 
-    microbit.display.clear()
-
     while True:
+        microbit.display.clear()
         if microbit.button_a.was_pressed():
-            while True:
+            if microbit.button_b.is_pressed():
+                microbit.display.clear()
+                return
+            else:
                 workcycle()
+            if microbit.button_b.is_pressed():
+                microbit.display.clear()
+                return
+            else:
                 restcycle()
-            
-waiting()
+
+while True:
+    waiting()
